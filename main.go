d4427f4245c239bc2b16eb99f0105dd3afb194a4
Alexander A. Klimov <grandmaster@al2klimov.de>
2018-08-03 19:56:06 +0200

    Evaluate what to do

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+func main() {
+	if err := runAgent(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var apiPkgMgrActions = map[pkgMgrAction]string{
+	pkgMgrInstall:   "install",
+	pkgMgrUpdate:    "update",
+	pkgMgrConfigure: "configure",
+	pkgMgrRemove:    "remove",
+	pkgMgrPurge:     "purge",
+}
+
+func runAgent() error {
+	tasks, errWIUA := (&apt{}).whatIfUpgradeAll()
+	if errWIUA != nil {
+		return errWIUA
+	}
+
+	apiTasks := make([]interface{}, len(tasks))
+	apiTaskIdx := 0
+
+	for task := range tasks {
+		record := map[string]interface{}{
+			"package": task.packageName,
+			"action":  apiPkgMgrActions[task.action],
+		}
+
+		if task.fromVersion != "" {
+			record["from_version"] = task.fromVersion
+		}
+
+		if task.toVersion != "" {
+			record["to_version"] = task.toVersion
+		}
+
+		apiTasks[apiTaskIdx] = record
+		apiTaskIdx++
+	}
+
+	jsn, errJM := json.Marshal(apiTasks)
+	if errJM != nil {
+		return errJM
+	}
+
+	_, errPL := fmt.Println(string(jsn))
+	return errPL
+}
