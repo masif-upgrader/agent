62e1d617175876e77acfd587e7969749206b08da
Alexander A. Klimov <grandmaster@al2klimov.de>
2018-10-15 11:55:20 +0200

    Update code generator

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
-//go:generate go run gen1.go
-//go:generate go run gen2.go GithubcomAl2klimovGogeneratedeps.go
+//go:generate go run vendor/github.com/Al2Klimov/go-gen-source-repos/main.go github.com/masif-upgrader/agent
 
 package main
 
@@ -7,10 +6,9 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	_ "github.com/Al2Klimov/go-generate-deps"
+	_ "github.com/Al2Klimov/go-gen-source-repos"
 	"github.com/go-ini/ini"
 	"github.com/masif-upgrader/common"
-	_ "github.com/masif-upgrader/common"
 	"os"
 	"strings"
 	"syscall"
@@ -190,7 +188,7 @@ func loadCfg() (config *settings, err error) {
 	if *cfgFile == "" {
 		fmt.Printf(
 			"For the terms of use, the source code and the authors\nsee the projects this program is assembled from:\n\n  %s\n\n",
-			strings.Join(GithubcomMasif_upgraderCommon, "\n  "),
+			strings.Join(GithubcomAl2klimovGo_gen_source_repos, "\n  "),
 		)
 
 		return nil, errors.New("config file missing")
