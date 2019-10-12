f6e802ad1b028019a6c2c360c85b32cb3754e298
Alexander A. Klimov <grandmaster@al2klimov.de>
2019-10-12 14:08:18 +0200

    /var/run/masif-upgrader-agent/rest.s: add GET /v1/load
    refs #2

--- rest.go
+++ rest.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+	"github.com/masif-upgrader/common"
 	log "github.com/sirupsen/logrus"
 	"net"
 )
@@ -16,9 +18,27 @@ func startRestServer(sock string) (*iris.Application, error) {
 
 	app := iris.New()
 
-	// TODO
+	app.Get("/v1/load", getV1Load)
 
 	go app.Run(iris.Listener(server), iris.WithoutStartupLog)
 
 	return app, nil
+}
+
+func getV1Load(context context.Context) {
+	context.JSON(&struct {
+		Install   [3]float64 `json:"install"`
+		Update    [3]float64 `json:"update"`
+		Configure [3]float64 `json:"configure"`
+		Remove    [3]float64 `json:"remove"`
+		Purge     [3]float64 `json:"purge"`
+		Error     [3]float64 `json:"error"`
+	}{
+		actionsStats[common.PkgMgrInstall].queryLoad(),
+		actionsStats[common.PkgMgrUpdate].queryLoad(),
+		actionsStats[common.PkgMgrConfigure].queryLoad(),
+		actionsStats[common.PkgMgrRemove].queryLoad(),
+		actionsStats[common.PkgMgrPurge].queryLoad(),
+		errorStats.queryLoad(),
+	})
 }
